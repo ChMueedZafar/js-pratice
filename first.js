81203ab0e0f15b85cc674a5b1322117e2ccf3ce2
ChMueedZafar <chmueedzafar@gmail.com>
2024-03-15 00:33:21 +0500

    second commit

--- first.js
+++ first.js
@@ -1,1 +1,36 @@
-console.log("appna collage");
+// variables pratice
+{
+    const a = 5;
+    console.log(a)
+}
+{
+    let b = 10;
+    console.log(b);
+}
+{
+    var c = 15;
+    console.log(c);
+}
+//#data type
+//1- primitive data type
+{
+let age = 18;
+console.log(age);
+}
+{
+    isfolow = true;
+}
+let x= null;
+
+let g = BigInt("123");
+let y =  Symbol("hello")
+// 2- non-primtive or objects
+const student ={
+    fulname : "mueed zafar",
+    age : 18,
+    cgpa : 8.2,
+    ispass: true,
+};
+student["age"] = student["age"]+1;
+
+console.log(student.age);
