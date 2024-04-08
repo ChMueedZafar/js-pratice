9bf0ef760366ce00b41ac055f3917f752e40251e
ChMueedZafar <chmueedzafar@gmail.com>
2024-04-09 00:11:44 +0500

    if and else conditions

--- first.js
+++ first.js
@@ -1,76 +1,109 @@
-// variables pratice
-{
-    const a = 5;
-    console.log(a)
+// // variables pratice
+// {
+//     const a = 5;
+//     console.log(a)
+// }
+// {
+//     let b = 10;
+//     console.log(b);
+// }
+// {
+//     var c = 15;
+//     console.log(c);
+// }
+// //#data type
+// //1- primitive data type
+// {
+// let age = 18;
+// console.log(age);
+// }
+// {
+//     isfolow = true;
+// }
+// let x= null;
+
+// let g = BigInt("123");
+// let y =  Symbol("hello")
+// // 2- non-primtive or objects
+// const student ={
+//     fulname : "mueed zafar",
+//     age : 18,
+//     cgpa : 8.2,
+//     ispass: true,
+// };
+// student["age"] = student["age"]+1;
+
+// console.log(student.age);
+// // pratice 1
+// const product = {
+//     title:"Ball pen",
+//     ratting: 4,
+//     offer: 5,
+//     price:270,
+// };
+// console.log(product)
+// const porfile = {
+//    username:"@mueedzafar",
+//    isfolow: false,
+//    followers: 123,
+//    following:123
+// };
+// console.log(porfile)
+// // comments
+// console.log("hello world!")
+// // operators in js
+// // 1- artimetice operator
+// let a = 5;
+// let b = 2;
+
+// console.log("a+b",a + b);
+// console.log("a-b",a - b);
+// console.log("a*b",a * b);
+// console.log("a/b",a / b);
+// console.log("a%b",a % b);
+// console.log("a**b",a ** b);
+
+// console.log("Function")
+
+// // logic operators
+// let z = 3;
+// let v = 5;
+// console.log("!(3 < 5)=",!(z < v));
+
+// if and if else comditions
+
+// let num = prompt("entre a number");
+
+// if (num % 5 === 0){
+//   console.log(num, "is multiple of 5");  
+// }else{
+//     console.log(num ,"is not multiple of 5");
+// }
+
+let scor =prompt("entre a number 0-100")
+let grade;
+
+
+if (scor>= 90 && scor <= 100){
+    grade ="A"
 }
-{
-    let b = 10;
-    console.log(b);
+else if(scor>=80 && scor <= 89){
+    grade ="B"
 }
-{
-    var c = 15;
-    console.log(c);
+else if(scor>=70 && scor <= 79){
+    grade ="C"
 }
-//#data type
-//1- primitive data type
-{
-let age = 18;
-console.log(age);
+else if(scor>=60 && scor <= 69){
+    grade ="D"
 }
-{
-    isfolow = true;
+else if(scor>=50 && scor <= 59){
+    grade ="E"
 }
-let x= null;
+else if(scor>=0 && scor <= 49){
+    grade ="F"
+}
 
-let g = BigInt("123");
-let y =  Symbol("hello")
-// 2- non-primtive or objects
-const student ={
-    fulname : "mueed zafar",
-    age : 18,
-    cgpa : 8.2,
-    ispass: true,
-};
-student["age"] = student["age"]+1;
-
-console.log(student.age);
-// pratice 1
-const product = {
-    title:"Ball pen",
-    ratting: 4,
-    offer: 5,
-    price:270,
-};
-console.log(product)
-const porfile = {
-   username:"@mueedzafar",
-   isfolow: false,
-   followers: 123,
-   following:123
-};
-console.log(porfile)
-// comments
-console.log("hello world!")
-// operators in js
-// 1- artimetice operator
-let a = 5;
-let b = 2;
-
-console.log("a+b",a + b);
-console.log("a-b",a - b);
-console.log("a*b",a * b);
-console.log("a/b",a / b);
-console.log("a%b",a % b);
-console.log("a**b",a ** b);
-
-console.log("Function")
-
-// logic operators
-let z = 3;
-let v = 5;
-console.log("!(3 < 5)=",!(z < v));
-
-
+console.log("According to your scor your grade was"+ grade);
 
 
 
